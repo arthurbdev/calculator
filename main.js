9993eea1cf32584c30c79b35736ff451a99dc4fb
Arthur Bais <artb@tutamail.com>
2023-06-17 21:12:17 +0300

    Add click events for buttons and functions for entering and evaluating numbers

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+const primDisplay = document.querySelector('#primary-display')
+const secondDisplay = document.querySelector('#secondary-display')
+const numButtons = document.querySelector('.number-buttons').querySelectorAll('button')
+const operatorButtons = document.querySelector('.operator-buttons').querySelectorAll('button')
+const equalbtn = document.querySelector('#equalbtn')
+
+let resetScreen = false
+let operand = null
+let firstNum = secondNum = ''
+
+
+numButtons.forEach(btn => btn.addEventListener('click', () => enterNumber(btn.textContent)))
+operatorButtons.forEach(btn => btn.addEventListener('click', () => enterOperation(btn.textContent)))
+
+equalbtn.addEventListener('click', evaluate)
+
+function enterOperation(operator) {
+  if(operand !== null) evaluate()
+  firstNum = primDisplay.textContent
+  operand = operator
+  secondDisplay.textContent = firstNum + " " + operand
+  resetScreen = true
+}
+
+function evaluate() {
+  secondNum = primDisplay.textContent
+  primDisplay.textContent = operate(operand, firstNum, secondNum)
+  secondDisplay.textContent = `${firstNum} ${operand} ${secondNum} =`
+  operand = null
+  resetScreen = true
+}
+
+function enterNumber(num) {
+  if(primDisplay.textContent === '0' || resetScreen) clearPrimDisplay()
+  primDisplay.textContent += num
+}
+
+function clearPrimDisplay(){
+  primDisplay.textContent = ''
+  resetScreen = false
+}
+
 add = (num1,num2) => num1 + num2
 subtract = (num1,num2) => num1 - num2
 multiply = (num1,num2) => num1 * num2
