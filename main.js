3b1609de582b0af061825e622cd7e2d02f507eba
Arthur Bais <artb@tutamail.com>
2023-06-17 18:45:02 +0300

    Add operate function that takes an operator and two numbers and evaluates them based on the operator

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ add = (num1,num2) => num1 + num2
 subtract = (num1,num2) => num1 - num2
 multiply = (num1,num2) => num1 * num2
 divide = (num1,num2) => num1 / num2
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2)
+    case '-':
+      return subtract(num1, num2)
+    case '/':
+      return divide(num1, num2)
+    case '*':
+      return multiply(num1, num2)
+  }
+}
