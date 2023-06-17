aaf219564165542fa8f33ef6922091dbf2b8a925
Arthur Bais <artb@tutamail.com>
2023-06-17 21:11:27 +0300

    main.js operate function convert arguments to number; add if statement for divisions by null

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ multiply = (num1,num2) => num1 * num2
 divide = (num1,num2) => num1 / num2
 
 function operate(operator, num1, num2) {
+  num1 = Number(num1)
+  num2 = Number(num2)
   switch(operator) {
     case '+':
       return add(num1, num2)
     case '-':
       return subtract(num1, num2)
     case '/':
+      if(num2 === 0) return null
       return divide(num1, num2)
     case '*':
       return multiply(num1, num2)
